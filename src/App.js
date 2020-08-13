100e6337d84a55b67c96befe4123b5218c7811c2
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-08-13 13:32:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {Button,Form,FormGroup,Input, Label} from 'reactstrap';
+import {FacebookLoginButton,GoogleLoginButton,GithubLoginButton} from 'react-social-login-buttons'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="login-form">
+    <h1>
+     <span className="font-weight-bold">Kickstartx</span>.com
+      </h1>
+      <h2 className="text-center">Welcome</h2>
+      <FormGroup>
+        <Label>Email</Label>
+        <Input type="email" placeholder="Enter your Email"/>
+      </FormGroup>
+      <FormGroup>
+        <Label>Password</Label>
+        <Input type="password" placeholder="password"/>
+      </FormGroup>
+      <Button className="btn-lg btn-light btn-block">Login</Button>
+      <div className="text-center pt-3">
+           Or continue with Facebook or Google or Github
+      </div>
+      <FacebookLoginButton className="mt-3 mb-3"/>
+      <GoogleLoginButton className="mt-3 mb-3"/>
+      <GithubLoginButton className="mt-3 mb-3" />
+      <div className="text-center">
+        <a href="/sign-up">Sign up</a>
+        <span className="p-2">|</span>
+        <a href="/forgot password?">Forgot Password</a>
+      </div>
+    </Form>
   );
 }
 
